a950c3e6f3661936d516e5de6171e20461a8c1f6
jkling <jordi.kling@akquinet.de>
2024-12-11 12:07:56 +0100

    PART_ONE and PART_TWO now in PuzzleSpec namespace

--- src/test/kotlin/de/akquinet/jkl/aoc24/PuzzleSpec.kt
+++ src/test/kotlin/de/akquinet/jkl/aoc24/PuzzleSpec.kt
@@ -3,7 +3,11 @@ package de.akquinet.jkl.aoc24
 import io.kotest.core.spec.style.FunSpec
 import java.io.File
 
+@Suppress("PropertyName")
 abstract class PuzzleSpec(number: Int, body: PuzzleSpec.() -> Unit) : FunSpec() {
+  val PART_ONE = "part one"
+  val PART_TWO = "part two"
+
   private val inputFile = File(javaClass.getResource("/$number.txt")!!.toURI())
 
   fun readInputAsText(): String = inputFile.readText()
@@ -14,6 +18,3 @@ abstract class PuzzleSpec(number: Int, body: PuzzleSpec.() -> Unit) : FunSpec() 
     body()
   }
 }
-
-const val PART_ONE = "part one"
-const val PART_TWO = "part two"
